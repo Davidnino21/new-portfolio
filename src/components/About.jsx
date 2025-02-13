b5ad301ed2099039c9c32f1c27a1455491af4b17
Davidnino21 <davidnino_21@yahoo.com>
2025-02-13 13:13:25 -0800

    Started about component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="content">
+      <card>
+        <p>
+          As an aspiring Junior Front-End Web Developer based in Northern
+          California, I am passionate about creating engaging and user-friendly
+          digital experiences. With a focus on blending design and
+          functionality, I strive to bring creative visions to life through
+          clean and efficient code.
+        </p>
+      </card>
+    </div>
+  );
+}
+
+export default About;
