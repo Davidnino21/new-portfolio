7b6b79bbeea29d9657b4ee76d7a645e4ec7d9919
Davidnino21 <davidnino_21@yahoo.com>
2025-02-14 14:31:00 -0800

    deleted old photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,7 @@ function Projects() {
     <div className="projects">
       <h2>My Work</h2>
       <div className="projects-photos">
-        <img src="/codingQuiz.png" alt="" />
-        <img src="/groovy.png" alt="" />
-        <img src="/horiseon.png" alt="" />
-        <img src="/savory.png" alt="" />
-        <img src="/textEditor.png" alt="" />
-        <img src="/workday.png" alt="" />
+        
       </div>
     </div>
   );
