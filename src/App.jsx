904c8c6b8c473896d65f35be74c275b5cdec5713
Davidnino21 <davidnino_21@yahoo.com>
2025-02-19 16:34:18 -0800

    added skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <About />
       </div>
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
---
+++ src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div className="skill-content">
+      <div className="skill-row">
+        <h2>HTML</h2>
+        <h2>CSS</h2>
+        <h2>JavaScript</h2>
+        <h2>React</h2>
+        <h2>Responsive Design</h2>
+        <h2>RESTful APIs</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
